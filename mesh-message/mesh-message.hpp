aed9cff61eb6d97dae2dfd3e439d62c77f03f20a
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-11-18 11:14:59 -0600

    Create mesh-message.hpp

---
+++ mesh-message/mesh-message.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+// C++11 lest unit testing framework
+#include "lest.hpp"
+
+using namespace std;
+
+vector<string> getPath(const unordered_map<string, vector<string>>& graph,
+                       const string& startNode, const string& endNode)
+{
+    // find the shortest route in the network between the two users
+    
+
+    return  vector<string>();
+}
